82cf8291a266a944404a6d039f3592b722300601
heri <40530246+heri@users.noreply.github.com>
2021-05-16 18:43:38 +0700

    Update login_logout.js

--- js/login_logout.js
+++ js/login_logout.js
@@ -6,7 +6,32 @@ function start_quantxi_btn() {
       open("img/ailogo2.png","_self");
   }
   
-  
+  var endpoint = "http://localhost/rasio_server/api/get.php";
+  var api_key = $("#api_key").val();
+    $.ajax({
+        type: "GET",
+        url: endpoint,
+        headers:{
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-API-KEY": api_key
+        },
+        dataType: 'json',
+        success: function(result){
+        if (result.status == "success") {
+            alert(`selamat datang point72`); //setting diserver u/tampilkan message nama user api
+            sessionStorage.setItem("api", api_key);
+            open("dashboard.html","_self");
+        }
+        else {
+            alert(`gagal koneksi, periksa api key anda`);
+            return false;
+          }
+        },
+        error: function() {
+            alert(`gagal koneksi, periksa api key anda`);
+            return false;
+        }
+    })
   }
   
 //Function Logout
