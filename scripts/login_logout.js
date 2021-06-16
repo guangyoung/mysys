3cbbc06523d8f6e91fb74ae04ecc04ede61d1333
heri <40530246+heri@users.noreply.github.com>
2021-06-16 13:27:31 +0700

    img to images

--- scripts/login_logout.js
+++ scripts/login_logout.js
@@ -3,7 +3,7 @@ function start_quantxi_btn() {
   
   let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
   if (width < 1200) {
-      open("img/ailogo2.png","_self");
+      open("images/ailogo2.png","_self");
   }
   
   var api_key = $("#api_key").val();
