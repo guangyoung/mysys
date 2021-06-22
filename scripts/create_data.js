d2acffa6ed24141c05d84083fdb00a2a3d1d6673
heri <40530246+heri@users.noreply.github.com>
2021-06-22 16:23:34 +0700

    Update create_data.js

--- scripts/create_data.js
+++ scripts/create_data.js
@@ -182,8 +182,10 @@ function appendLeadingZeroes(n){
           while (startDate <= endDate) {
             as_arr = [];
             dtt = appendLeadingZeroes(startDate.getMonth()+1) + "/" + appendLeadingZeroes(startDate.getDate()) + "/" + startDate.getFullYear();
-            as_arr.push(dtt);          
-            for (y=0; y<30; y++) {
+            as_arr.push(dtt);
+            var y =0;          
+            while (y<30) {
+              y++;
               var idx = asset_portfolio_yahoo[y].data.date.indexOf(dtt);
               if(idx == -1) {//jika idx tidak ditemukan
                   as_arr.push(port_data[port_data.length-1][y+1]); //masukkan harga sebelumnya
