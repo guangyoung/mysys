16842deebfd42272cebbdfb491c6b71529579f50
heri <40530246+heri@users.noreply.github.com>
2021-07-10 18:59:17 +0700

    Update create_data.js

--- scripts/create_data.js
+++ scripts/create_data.js
@@ -118,7 +118,7 @@ function appendLeadingZeroes(n){
 }
 
 // tombol proses data utk data from yahoo and files
- function process_data_yahoo() {
+function process_data_yahoo() {
       if(asset_portfolio_yahoo.length < 30) {
           alert('total asset kurang dari 30');
           return false;
@@ -145,7 +145,7 @@ function appendLeadingZeroes(n){
             dtt = appendLeadingZeroes(startDate.getMonth()+1) + "/" + appendLeadingZeroes(startDate.getDate()) + "/" + startDate.getFullYear();
             as_arr.push(dtt);          
             for (y=0; y<30; y++) { //CEK BAGAIMANA PROSES INI BISA CEPAT....PENTIIIING !!!!!!
-              var idx = asset_portfolio_yahoo[y].data.date.search(dtt);
+              idx = asset_portfolio_yahoo[y].data.date.indexOf(dtt);
               if(idx == -1) {//jika idx tidak ditemukan
                   as_arr.push(port_data[port_data.length-1][y+1]); //masukkan harga sebelumnya
               } else {
