198923689a49ad0e895991a9a4f0e0013ea2b4f1
heri <40530246+heri@users.noreply.github.com>
2021-07-11 20:19:57 +0700

    Update setting.js

--- scripts/setting.js
+++ scripts/setting.js
@@ -4,21 +4,13 @@ var initialequity;
 var bidaskspread;
 var commisionshare;
 var interestrate;
-var regTmargin;
-var maintmargin;
-var portsize;
-var minpost;
 
 function test_setting_submit_btn() {
-    
+
     initialequity = $("#initial_equity").val();
     bidaskspread = $("#bid_ask_spread").val();
     commisionshare = $("#commision_share").val();
     interestrate = $("#interest_rate").val();
-    regTmargin = $("#regT_margin").val();
-    maintmargin = $("#maint_margin").val();
-    portsize = $("#port_size").val();
-    minpost = $("#min_post").val();
 
     alert('Setting berhasil disimpan');
   
