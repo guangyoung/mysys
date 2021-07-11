edbf61b168d7bc2a2aba7dbe01fbe0aa3847c9c2
heri <40530246+heri@users.noreply.github.com>
2021-07-11 09:38:20 +0700

    Update create_data.js

--- scripts/create_data.js
+++ scripts/create_data.js
@@ -144,12 +144,14 @@ function process_data_yahoo() {
             as_arr = [];
             dtt = appendLeadingZeroes(startDate.getMonth()+1) + "/" + appendLeadingZeroes(startDate.getDate()) + "/" + startDate.getFullYear();
             as_arr.push(dtt);          
-            for (y=0; y<30; y++) { //CEK BAGAIMANA PROSES INI BISA CEPAT....PENTIIIING !!!!!!
-              let idx = asset_portfolio_yahoo[y].data.date.indexOf(dtt);
-              if(idx == -1) {//jika idx tidak ditemukan
-                as_arr.push(port_data[port_data.length-1][y+1]); //masukkan harga sebelumnya
-              } else {
-                as_arr.push(asset_portfolio_yahoo[y].data.price[idx]); //jika idx ketemu masukkan harga berdasarkan idx
+            for (y=0; y<3; y++) { //CEK BAGAIMANA PROSES INI BISA CEPAT....PENTIIIING !!!!!!
+              for (i=y*10; i<((y+1)*10); y++) { //CEK BAGAIMANA PROSES INI BISA CEPAT....PENTIIIING !!!!!!
+                let idx = asset_portfolio_yahoo[i].data.date.indexOf(dtt);
+                if(idx == -1) {//jika idx tidak ditemukan
+                  as_arr.push(port_data[port_data.length-1][i+1]); //masukkan harga sebelumnya
+                } else {
+                  as_arr.push(asset_portfolio_yahoo[i].data.price[idx]); //jika idx ketemu masukkan harga berdasarkan idx
+                }
               }
             }
             port_data.push(as_arr);
