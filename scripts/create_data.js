806b547a433374c42f7334743ee73f41f7cd2e67
heri <40530246+heri@users.noreply.github.com>
2021-07-11 09:19:05 +0700

    Update create_data.js

--- scripts/create_data.js
+++ scripts/create_data.js
@@ -140,11 +140,11 @@ function process_data_yahoo() {
           var startDate=new Date(Math.max.apply(null,startdates));
           var endDate=new Date(Math.min.apply(null,enddates));
           var as_arr = new Array();
-          while (startDate <= endDate) {
+          while (startDate !== endDate) {
             as_arr = [];
             dtt = appendLeadingZeroes(startDate.getMonth()+1) + "/" + appendLeadingZeroes(startDate.getDate()) + "/" + startDate.getFullYear();
             as_arr.push(dtt);          
-            for (y=0; y<10; y++) { //CEK BAGAIMANA PROSES INI BISA CEPAT....PENTIIIING !!!!!!
+            for (y=0; y<30; y++) { //CEK BAGAIMANA PROSES INI BISA CEPAT....PENTIIIING !!!!!!
               let idx = asset_portfolio_yahoo[y].data.date.indexOf(dtt);
               if(idx == -1) {//jika idx tidak ditemukan
                 as_arr.push(port_data[port_data.length-1][y+1]); //masukkan harga sebelumnya
