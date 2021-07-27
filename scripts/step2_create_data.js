be7d6712cbe66532d7fb01a4a5c048f8ba6528af
heri <40530246+heri@users.noreply.github.com>
2021-07-27 10:28:34 +0700

    Update step2_create_data.js

--- scripts/step2_create_data.js
+++ scripts/step2_create_data.js
@@ -80,7 +80,7 @@
           const proxyurl = "https://cors-proxy.htmldriven.com/?url=";
           const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+tickere+"?symbol="+tickere+"&period1=0&period2=9999999999&interval=1d";
 
-          $.getJSON(urls, function(result){
+          $.getJSON(proxyurl+urls, function(result){
             var yahoo_data = result;
             let length_tm = yahoo_data.chart.result[0].timestamp.length;
             for(i=0; i<length_tm; i++) {
