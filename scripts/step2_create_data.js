7027a245e816d7fdcf98e3cca30ff90a878639aa
heri <40530246+heri@users.noreply.github.com>
2021-07-27 12:50:04 +0700

    Update step2_create_data.js

--- scripts/step2_create_data.js
+++ scripts/step2_create_data.js
@@ -86,7 +86,7 @@
             let length_tm = yahoo_data.chart.result[0].timestamp.length;
             for(i=0; i<length_tm; i++) {
             var data_date = new Date(yahoo_data.chart.result[0].timestamp[i] * 1000);
-            console.log(yahoo_data.chart.result[0].indicators.quote[0].adjclose[0]);
+            console.log(yahoo_data.chart.result[0].indicators.quote);
             var data_price = yahoo_data.chart.result[0].indicators.adjclose[0].adjclose[i];
             console.log(data_price);
             as_data_date.push(
