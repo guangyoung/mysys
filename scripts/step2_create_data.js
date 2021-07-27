7f813cb7c184e0e27f458027dd6c481167f7e976
heri <40530246+heri@users.noreply.github.com>
2021-07-27 14:07:10 +0700

    Update step2_create_data.js

--- scripts/step2_create_data.js
+++ scripts/step2_create_data.js
@@ -82,7 +82,7 @@
 
           $.getJSON(proxyurl+urls, function(result){
             console.log(result);
-            var yahoo_data = JSON.parse(result.contents);
+            var yahoo_data = result;
             let length_tm = yahoo_data.chart.result[0].timestamp.length;
             for(i=0; i<length_tm; i++) {
             var data_date = new Date(yahoo_data.chart.result[0].timestamp[i] * 1000);
