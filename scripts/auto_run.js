d886675d35d019067579b54ff9091f518305ac8c
heri <40530246+heri@users.noreply.github.com>
2021-08-11 14:58:49 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -6,26 +6,35 @@ var interestrate = 0.02;
 
 //data
 
-function autorun() {
+async function autorun() {
     Papa.parse("dataset/stock_tickers_list.csv", {
         download: true,
         header: false,
         complete: function(result) {
-        // console.log(result.data);
         for(i=1; i<2; i++) {
             const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
             const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[i][2]+"?symbol="+result.data[i][2]+"&period1=0&period2=9999999999&interval=1d";
-
             $.getJSON(proxyurl+urls, function(data){
-                // console.log(result.chart.result[0].indicators.adjclose[0].adjclose);
-                exchange = result.data[i][1];
-                ticker = result.data[i][2];
-                decrp = result.data[i][3];
-                edo_data = data.chart.result[0].indicators.adjclose[0].adjclose;
-                console.log(exchange);
-                console.log(ticker);  
-                console.log(decrp);  
-                console.log(edo_data);                
+                if(data.length>3000) {
+                    historical_data = {
+                        exchange: result.data[i][1],
+                        ticker: result.data[i][2],
+                        description: result.data[i][3],
+                        data: data.chart.result[0].indicators.adjclose[0].adjclose
+                    }              
+                    
+                    await $.ajax({
+                        type: "POST",
+                        url: "https://api.quantxi.com/add_data",
+                        data: historical_data,             
+                        dataType: 'json',
+                        // success: function(result){ 
+                        //     if (result.status == "success") {
+                       
+                        //     }         
+                        // }
+                    })
+                }                
             });
         }        
         }
