49a528e97457d07a1c6dd4da9bbaa63706d64823
heri <40530246+heri@users.noreply.github.com>
2021-08-11 15:38:48 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -14,23 +14,25 @@ function autorun() {
         for(i=1; i<30; i++) {
             const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
             const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[i][2]+"?symbol="+result.data[i][2]+"&period1=0&period2=9999999999&interval=1d";
+            const dat;
             $.getJSON(proxyurl+urls, function(data){
-                if(data.chart.result[0].indicators.adjclose[0].adjclose.length>3000) {
-                    historical_data = {
-                        exchange: result.data[i][1],
-                        ticker: result.data[i][2],
-                        description: result.data[i][3],
-                        data: data.chart.result[0].indicators.adjclose[0].adjclose.toString()
-                    }              
-                    console.log(historical_data);
-                    $.ajax({
-                        type: "POST",
-                        url: "https://api.quantxi.com/add_data",
-                        data: historical_data,             
-                        dataType: 'json'
-                    })
-                }                
+                dat = data.chart.result[0].indicators.adjclose[0].adjclose;
             });
+            if(dat>3000) {
+                historical_data = {
+                    exchange: result.data[i][1],
+                    ticker: result.data[i][2],
+                    description: result.data[i][3],
+                    data: dat.toString()
+                }              
+                console.log(historical_data);
+                $.ajax({
+                    type: "POST",
+                    url: "https://api.quantxi.com/add_data",
+                    data: historical_data,             
+                    dataType: 'json'
+                })
+            }     
         }        
         }
     });
