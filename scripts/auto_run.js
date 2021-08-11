3b17e6fe356266d8694bf2101fc1f8089a49cb0c
heri <40530246+heri@users.noreply.github.com>
2021-08-11 21:58:26 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -13,29 +13,27 @@ function autorun() {
         complete: function(result) {
             
             for(i=0; i<3; i++) {
-                eoddata = [];
                 const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
                 const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[i].Symbol+"?symbol="+result.data[i].Symbol+"&period1=0&period2=9999999999&interval=1d";
                 $.getJSON(proxyurl+urls, function(data){ 
-
+                    eoddata = [];
                     eoddata.push({data: data.chart.result[0].indicators.adjclose[0].adjclose}); 
-                    historical_data = {
-                        exchange: "NYSE",
-                        ticker: result.data[i].Symbol,
-                        description: "des",
-                        data: eoddata
-                    }              
-                    console.log(historical_data);
-                    $.ajax({
-                        type: "POST",
-                        url: "https://api.quantxi.com/add_data",
-                        data: historical_data,             
-                        dataType: 'json'
-                    })  
                                 
                 });
 
-                
+                historical_data = {
+                    exchange: "NYSE",
+                    ticker: result.data[i].Symbol,
+                    description: "des",
+                    data: eoddata
+                }              
+                console.log(historical_data);
+                $.ajax({
+                    type: "POST",
+                    url: "https://api.quantxi.com/add_data",
+                    data: historical_data,             
+                    dataType: 'json'
+                })  
             }                  
         }
     });
