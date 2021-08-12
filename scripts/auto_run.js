8485472f633a8d292dda8feab21b4bd86353c89d
heri <40530246+heri@users.noreply.github.com>
2021-08-12 14:59:10 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -14,29 +14,29 @@ function autorun() {
         download: true,
         header: true,
         complete: function(result) {               
-            for(i=0; i<10; i++) {
-                exch_list.push(result.data[i].Exchange);
-                tick_list.push(result.data[i].Symbol);  
-                des_list.push(result.data[i].Description);                 
-            } 
-            tl.push({exchanges: exch_list, ticker: tick_list, description: des_list});
-            console.log(tl.exchanges);
-            console.log(exch_list);
+            // for(i=0; i<10; i++) {
+            //     exch_list.push(result.data[i].Exchange);
+            //     tick_list.push(result.data[i].Symbol);  
+            //     des_list.push(result.data[i].Description);                 
+            // } 
+            // tl.push({exchanges: exch_list, ticker: tick_list, description: des_list});
+            // console.log(tl.exchanges);
+            // console.log(exch_list);
             for(i=0; i<3; i++) {
-                let exchange = tl[0].exchange[i];
-                let tickere = tl[0].ticker[i];
-                let descrip = tl[0].description[i];
+                // let exchange = tl[0].exchange[i];
+                // let tickere = tl[0].ticker[i];
+                // let descrip = tl[0].description[i];
                 
                 var data_price;
                 const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
-                const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+tickere+"?symbol="+tickere+"&period1=0&period2=9999999999&interval=1d";
+                const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[i].Symbol+"?symbol="+result.data[i].Symbol+"&period1=0&period2=9999999999&interval=1d";
                 $.getJSON(proxyurl+urls, function(result){                 
                     data_price = result.chart.result[0].indicators.adjclose[0].adjclose;
                 });       
                 historical_data = {
-                    exchange: exchange,
-                    ticker: tickere,
-                    description: descrip,
+                    exchange: result.data[i].Exchange,
+                    ticker: result.data[i].Symbol,
+                    description: result.data[i].Description,
                     data: data_price
                 }              
                 console.log(historical_data);
