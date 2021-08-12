75a1683fe068730bd83300185340550882e95a4b
heri <40530246+heri@users.noreply.github.com>
2021-08-12 09:46:20 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -5,37 +5,44 @@ var commisionshare = 0.01;
 var interestrate = 0.02;
 
 //data
-var eod = new Array();
+var tick_list = new Array();
 function autorun() {     
     Papa.parse("dataset/stock_tickers_list.csv", {
         download: true,
         header: true,
         complete: function(result) {            
             for(i=0; i<3; i++) {
-                var eoddata = new Array();
-                const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
-                const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[i].Symbol+"?symbol="+result.data[i].Symbol+"&period1=0&period2=9999999999&interval=1d";
-                $.getJSON(proxyurl+urls, function(data){ 
-                    eoddata.push(data.chart.result[0].indicators.adjclose[0].adjclose);
-                });
-                eod.push(eoddata);
-                console.log(eod);
-                historical_data = {
-                    exchange: "NYSE",
-                    ticker: result.data[i].Symbol,
-                    description: "des",
-                    data: eod[i]
-                }              
-                console.log(historical_data);
-                $.ajax({
-                    type: "POST",
-                    url: "https://api.quantxi.com/add_data",
-                    data: historical_data,             
-                    dataType: 'json'
-                })  
+                tick_list.push(result.data[i].Symbol);
             }                  
         }
     });
 
+    for(i=0; i<3; i++) {
+        let tickere = tick_list[i];
+        let as_data_price = new Array();
+        const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
+        const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+tickere+"?symbol="+tickere+"&period1=0&period2=9999999999&interval=1d";
+        $.getJSON(proxyurl+urls, function(result){ 
+            for(i=0; i<result.chart.result[0].indicators.adjclose[0].adjclose.length; i++) {
+                var data_price = result.chart.result[0].indicators.adjclose[0].adjclose;
+                as_data_price.push(
+                    data_price
+                );
+            }
+        });       
+        historical_data = {
+            exchange: "NYSE",
+            ticker: tickere,
+            description: "des",
+            data: as_data_price
+        }              
+        console.log(historical_data);
+        $.ajax({
+            type: "POST",
+            url: "https://api.quantxi.com/add_data",
+            data: historical_data,             
+            dataType: 'json'
+        }) 
+    }
     
 }
