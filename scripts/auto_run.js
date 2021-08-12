954d5ac958b14266bebf81d0a022e7296bc0b270
heri <40530246+heri@users.noreply.github.com>
2021-08-12 15:12:55 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -22,17 +22,19 @@ function autorun() {
             // tl.push({exchanges: exch_list, ticker: tick_list, description: des_list});
             // console.log(tl.exchanges);
             // console.log(exch_list);
+            var data_price = [];
             for(i=0; i<3; i++) {
                 // let exchange = tl[0].exchange[i];
                 // let tickere = tl[0].ticker[i];
                 // let descrip = tl[0].description[i];
                 
-                var data_price = [];
+                
                 const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
                 const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[i].Symbol+"?symbol="+result.data[i].Symbol+"&period1=0&period2=9999999999&interval=1d";
                 $.getJSON(proxyurl+urls, function(data){                 
                     data_price.push(data.chart.result[0].indicators.adjclose[0].adjclose);
-                });       
+                });     
+                console.log(data_price);  
                 historical_data = {
                     exchange: result.data[i].Exchange,
                     ticker: result.data[i].Symbol,
