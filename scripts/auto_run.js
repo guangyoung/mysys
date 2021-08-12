f83f47ddf57bc2d8300d2381282bc9a44969adbc
heri <40530246+heri@users.noreply.github.com>
2021-08-12 16:17:08 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -12,16 +12,16 @@ function autorun() {
         header: true,
         complete: function(result) {             
             for(i=0; i<3; i++) {
-                let as_data_price                
+                let as_data_price = new Array();                
                 const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
                 const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[i].Symbol+"?symbol="+result.data[i].Symbol+"&period1=0&period2=9999999999&interval=1d";
                 $.getJSON(proxyurl+urls, function(data){
                     let length_tm = data.chart.result[0].timestamp.length;
                     // for(i=0; i<length_tm; i++) {                 
-                        as_data_price = data.chart.result[0].indicators.adjclose[0].adjclose;
-                        // as_data_price.push(
-                        //     data_price
-                        // );
+                        var data_price = data.chart.result[0].indicators.adjclose[0].adjclose;
+                        as_data_price.push(
+                            data_price
+                        );
                     // }
                 });     
                 // console.log(as_data_price);  
@@ -29,7 +29,7 @@ function autorun() {
                     exchange: result.data[i].Exchange,
                     ticker: result.data[i].Symbol,
                     description: result.data[i].Description,
-                    data: as_data_price
+                    data: as_data_price[0]
                 }              
                 console.log(historical_data);
                 $.ajax({
