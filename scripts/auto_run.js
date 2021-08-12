901a0f5a7d3498f582cde50f4a696b32a73c9918
heri <40530246+heri@users.noreply.github.com>
2021-08-12 14:30:37 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -19,44 +19,32 @@ function autorun() {
                 tick_list.push(result.data[i].Symbol);  
                 des_list.push(result.data[i].Description);                 
             } 
+            tl.push({exchanges: exch_list, ticker: tick_list, description: des_list});
+            for(i=0; i<3; i++) {
+                let exchange = tl[0].exchange[i];
+                let tickere = tl[0].ticker[i];
+                let descrip = tl[0].description[i];
+                
+                var data_price;
+                const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
+                const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+tickere+"?symbol="+tickere+"&period1=0&period2=9999999999&interval=1d";
+                $.getJSON(proxyurl+urls, function(result){                 
+                    data_price = result.chart.result[0].indicators.adjclose[0].adjclose;
+                });       
+                historical_data = {
+                    exchange: exchange,
+                    ticker: tickere,
+                    description: descrip,
+                    data: data_price
+                }              
+                console.log(historical_data);
+                $.ajax({
+                    type: "POST",
+                    url: "https://api.quantxi.com/add_data",
+                    data: historical_data,             
+                    dataType: 'json'
+                }) 
+            }
         }
     });
-    tl.push({exchanges: exch_list, ticker: tick_list, description: des_list});
-    console.log(tl);
-    console.log(tl[0].exchanges[0]);
-        for(i=0; i<3; i++) {
-            let exchange = tl[0].exchange[i];
-            let tickere = tl[0].ticker[i];
-            let descrip = tl[0].description[i];
-            
-            var data_price;
-            // let as_data_price = new Array();
-            const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
-            const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+tickere+"?symbol="+tickere+"&period1=0&period2=9999999999&interval=1d";
-            $.getJSON(proxyurl+urls, function(result){ 
-                // var yahoo_data = result;
-                // console.log(yahoo_data);
-                // let length_tm = 1000;
-                // for(i=0; i<length_tm; i++) {
-                    data_price = result.chart.result[0].indicators.adjclose[0].adjclose;
-                    // as_data_price.push(
-                    //     data_price
-                    // );
-                // }
-            });       
-            historical_data = {
-                exchange: exchange,
-                ticker: tickere,
-                description: descrip,
-                data: data_price
-            }              
-            console.log(historical_data);
-            $.ajax({
-                type: "POST",
-                url: "https://api.quantxi.com/add_data",
-                data: historical_data,             
-                dataType: 'json'
-            }) 
-        }
-    
 }
