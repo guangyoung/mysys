4ce48fe953505778e2893c60c4e61948b0f6df29
heri <40530246+heri@users.noreply.github.com>
2021-08-12 09:27:46 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -5,27 +5,26 @@ var commisionshare = 0.01;
 var interestrate = 0.02;
 
 //data
-
+var eod = new Array();
 function autorun() {     
     Papa.parse("dataset/stock_tickers_list.csv", {
         download: true,
         header: true,
-        complete: function(result) {
-            
+        complete: function(result) {            
             for(i=0; i<3; i++) {
                 var eoddata = new Array();
                 const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
                 const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[i].Symbol+"?symbol="+result.data[i].Symbol+"&period1=0&period2=9999999999&interval=1d";
                 $.getJSON(proxyurl+urls, function(data){ 
                     eoddata.push(data.chart.result[0].indicators.adjclose[0].adjclose);
-                    console.log(eoddata);                                 
+                    eod.push(eoddata);                                
                 });
-
+                console.log(eod);
                 historical_data = {
                     exchange: "NYSE",
                     ticker: result.data[i].Symbol,
                     description: "des",
-                    data: eoddata
+                    data: eod[i]
                 }              
                 console.log(historical_data);
                 $.ajax({
