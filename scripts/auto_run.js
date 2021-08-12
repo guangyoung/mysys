46addc6facbdd667bc108437edb93ddfee73e787
heri <40530246+heri@users.noreply.github.com>
2021-08-12 09:29:20 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -17,8 +17,8 @@ function autorun() {
                 const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[i].Symbol+"?symbol="+result.data[i].Symbol+"&period1=0&period2=9999999999&interval=1d";
                 $.getJSON(proxyurl+urls, function(data){ 
                     eoddata.push(data.chart.result[0].indicators.adjclose[0].adjclose);
-                    eod.push(eoddata);                                
                 });
+                eod.push(eoddata);
                 console.log(eod);
                 historical_data = {
                     exchange: "NYSE",
