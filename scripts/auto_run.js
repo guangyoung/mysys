6102962236d3aa0cbe3f117bb62dc524fcef49d3
heri <40530246+heri@users.noreply.github.com>
2021-08-13 16:17:02 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -19,7 +19,7 @@ function autorun() {
             console.log(arr);  
             var stock_data = 0; 
             var i = 0;        
-            while(stock_data<30) {                
+            while(stock_data < 30) {                
                 let exchange= result.data[arr[i]].Exchange;
                 let ticker= result.data[arr[i]].Symbol;
                 console.log(exchange);
@@ -28,7 +28,7 @@ function autorun() {
                 const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
                 const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[arr[i]].Symbol+"?symbol="+result.data[arr[i]].Symbol+"&period1=0&period2=9999999999&interval=1d";
                 $.getJSON(proxyurl+urls, function(data){
-                    if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500) {
+                    if(data.chart.result[0].indicators.adjclose[0].adjclose.length>20) {
                         historical_data = {
                             exchange: exchange,
                             ticker: ticker,
