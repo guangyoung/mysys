51fd15f60d2a8cb37d3db512c205c09383f16b06
heri <40530246+heri@users.noreply.github.com>
2021-08-14 21:38:20 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -14,35 +14,29 @@ function autorun() {
             var arr = [];
             let i = 0;     
             for (i=0; i< 30; i++){
-                let r = Math.floor(Math.random() * 1000) + 1;
-                let dat = new Array();      
-                let exchange= result.data[r].Exchange;
-                let ticker= result.data[r].Symbol;
-                let description= result.data[r].Description; 
-                const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
-                const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[r].Symbol+"?symbol="+result.data[r].Symbol+"&period1=0&period2=9999999999&interval=1d";
-                let d = new Array(); 
+                var r = Math.floor(Math.random() * 1000) + 1;
                 if(arr.indexOf(r) === -1) {
-                   
-                    $.getJSON(proxyurl+urls, function(data){                                              
+                    let dat = new Array();      
+                    let exchange= result.data[r].Exchange;
+                    let ticker= result.data[r].Symbol;
+                    let description= result.data[r].Description; 
+                    const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
+                    const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[r].Symbol+"?symbol="+result.data[r].Symbol+"&period1=0&period2=9999999999&interval=1d";
+
+                    $.getJSON(proxyurl+urls, function(data){
                         if(data.chart.result !== null) {
                             if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500) {
-                                var data_price = data.chart.result[0].indicators.adjclose[0].adjclose;  
-                                // dat.push(
-                                //     data_price
-                                // ); 
+                                dat.push(data.chart.result[0].indicators.adjclose[0].adjclose);
                                 console.log(exchange);
                                 console.log(ticker);
                                 console.log(description);                
-                                stock_data.push({exchange: exchange, ticker: ticker, description: description, data: data_price});
+                                stock_data.push({exchange: exchange, ticker: ticker, description: description, data: dat});
                                 console.log(stock_data[stock_data.length-1].data[0].length);
-
                             }                        
                         } 
                     });
-                    
-                    arr.push(r);  
-                    i++;                 
+                arr.push(r);  
+                i++; 
                 }                
             }                           
                             
