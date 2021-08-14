1ca54c82b04eb0dc132538464c6f0bacff267910
heri <40530246+heri@users.noreply.github.com>
2021-08-14 18:47:40 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -25,7 +25,7 @@ function autorun() {
                 const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
                 const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[arr[i]].Symbol+"?symbol="+result.data[arr[i]].Symbol+"&period1=0&period2=9999999999&interval=1d";
                 $.getJSON(proxyurl+urls, function(data){
-                    // console.log(data);
+                    console.log(data);
                     if(data.chart.result !== null) {
                         sessionStorage.setItem("data", JSON.stringify(data.chart.result[0].indicators.adjclose[0].adjclose));
                     }
