51169de09e99c444515a24d5e280edf37c172be7
heri <40530246+heri@users.noreply.github.com>
2021-08-14 15:36:41 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -18,33 +18,33 @@ function autorun() {
             }            
             var i = 0;        
             while(stock_data.length < 30) {   
-                let dat = new Array();             
+                // let dat = new Array();             
                 let exchange= result.data[arr[i]].Exchange;
                 let ticker= result.data[arr[i]].Symbol;
                 let description= result.data[arr[i]].Description;             
                 const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
                 const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[arr[i]].Symbol+"?symbol="+result.data[arr[i]].Symbol+"&period1=0&period2=9999999999&interval=1d";
-                $.getJSON(proxyurl+urls, function(data){
-                    if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500) {
-                        dat.push(JSON.stringify(data.chart.result[0].indicators.adjclose[0].adjclose));
-                        // historical_data = {
-                        //     exchange: exchange,
-                        //     ticker: ticker,
-                        //     description: description,
-                        //     data: JSON.stringify(data.chart.result[0].indicators.adjclose[0].adjclose)
-                        // } 
-                        // console.log(historical_data);
+                let dat = $.getJSON(proxyurl+urls, function(data){});
+                    // if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500) {
+                    //     dat.push(JSON.stringify(data.chart.result[0].indicators.adjclose[0].adjclose));
+                    //     // historical_data = {
+                    //     //     exchange: exchange,
+                    //     //     ticker: ticker,
+                    //     //     description: description,
+                    //     //     data: JSON.stringify(data.chart.result[0].indicators.adjclose[0].adjclose)
+                    //     // } 
+                    //     // console.log(historical_data);
                        
-                        // $.ajax({
-                        //     type: "POST",
-                        //     url: "https://api.quantxi.com/add_data",
-                        //     data: historical_data,             
-                        //     dataType: 'json'
-                        // })    
-                    } else {
-                        return false;
-                    }
-                });
+                    //     // $.ajax({
+                    //     //     type: "POST",
+                    //     //     url: "https://api.quantxi.com/add_data",
+                    //     //     data: historical_data,             
+                    //     //     dataType: 'json'
+                    //     // })    
+                    // } else {
+                    //     return false;
+                    // }
+                // });
                 console.log(dat);
                 console.log(ticker);
                 stock_data.push({exchange: exchange, ticker: ticker, description: description, data: dat});
