467c1e2c78f54657cc6f5d02f1aa8e45d70d26aa
heri <40530246+heri@users.noreply.github.com>
2021-08-15 10:29:58 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -12,8 +12,8 @@ function autorun() {
         header: true,
         complete: function(result) {
             var arr = [];
-            let i = 0;     
-            while (i< 30){
+            // let i = 0;     
+            // while (i< 30){
                 var r = Math.floor(Math.random() * 1000) + 1;
                 if(arr.indexOf(r) === -1) {
                     let dat = new Array();      
@@ -22,22 +22,25 @@ function autorun() {
                     let description= result.data[r].Description; 
                     const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
                     const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[r].Symbol+"?symbol="+result.data[r].Symbol+"&period1=0&period2=9999999999&interval=1d";
-                    $.getJSON(proxyurl+urls, function(data){
-                        if(data.chart.result !== null) {
-                            if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500) {
-                                dat.push(data.chart.result[0].indicators.adjclose[0].adjclose);
-                                stock_data.push({exchange: exchange, ticker: ticker, description: description, data: dat});
-                                console.log(stock_data[stock_data.length-1].exchange);
-                                console.log(stock_data[stock_data.length-1].ticker);
-                                console.log(stock_data[stock_data.length-1].description);
-                                console.log(stock_data[stock_data.length-1].data[0].length);
-                            }                        
-                        } 
-                    });
+                    while(stock_data.length < 30) {
+                        $.getJSON(proxyurl+urls, function(data){
+                            if(data.chart.result !== null) {
+                                if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500) {
+                                    dat.push(data.chart.result[0].indicators.adjclose[0].adjclose);
+                                    stock_data.push({exchange: exchange, ticker: ticker, description: description, data: dat});
+                                    console.log(stock_data[stock_data.length-1].exchange);
+                                    console.log(stock_data[stock_data.length-1].ticker);
+                                    console.log(stock_data[stock_data.length-1].description);
+                                    console.log(stock_data[stock_data.length-1].data[0].length);
+                                }                        
+                            } 
+                        });
+                    }
+                    
                 arr.push(r);  
-                i++; 
+                // i++; 
                 }                
-            }                           
+            // }                           
                             
                 
                  
