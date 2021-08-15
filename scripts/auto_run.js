a122e3ba8f83a2549ef7a8ead8cc3c9487008364
heri <40530246+heri@users.noreply.github.com>
2021-08-15 10:22:44 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -6,45 +6,38 @@ var interestrate = 0.02;
 
 //data
 var stock_data = new Array(); 
-var arr = [];
 function autorun() {  
-    while (stock_data.length < 30) {
-        Papa.parse("dataset/stock_tickers_list.csv", {
-            download: true,
-            header: true,
-            complete: function(result) {
-               
-                let i = 0;     
-                // while (i < 50){
-                    var r = Math.floor(Math.random() * 1000) + 1;
-                    if(arr.indexOf(r) === -1) {
-                        let dat = new Array();      
-                        let exchange= result.data[r].Exchange;
-                        let ticker= result.data[r].Symbol;
-                        let description= result.data[r].Description; 
-                        const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
-                        const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[r].Symbol+"?symbol="+result.data[r].Symbol+"&period1=0&period2=9999999999&interval=1d";
-                        $.getJSON(proxyurl+urls, function(data){
-                            if(data.chart.result !== null) {
-                                if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500 && stock_data.length<30) {
-                                    dat.push(data.chart.result[0].indicators.adjclose[0].adjclose);
-                                    stock_data.push({exchange: exchange, ticker: ticker, description: description, data: dat});
-                                    console.log(stock_data[stock_data.length-1].exchange);
-                                    console.log(stock_data[stock_data.length-1].ticker);
-                                    console.log(stock_data[stock_data.length-1].description);
-                                    console.log(stock_data[stock_data.length-1].data[0].length);
-                                    arr.push(r); 
-                                }                        
-                            } 
-                        });
-                     
-                    // i++; 
-                    }                
-                }
-            });
-    }
-       
-                                           
+    Papa.parse("dataset/stock_tickers_list.csv", {
+        download: true,
+        header: true,
+        complete: function(result) {
+            var arr = [];
+            let i = 0;     
+            while (i< 30){
+                var r = Math.floor(Math.random() * 1000) + 1;
+                if(arr.indexOf(r) === -1) {
+                    let dat = new Array();      
+                    let exchange= result.data[r].Exchange;
+                    let ticker= result.data[r].Symbol;
+                    let description= result.data[r].Description; 
+                    const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
+                    const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[r].Symbol+"?symbol="+result.data[r].Symbol+"&period1=0&period2=9999999999&interval=1d";
+                    $.getJSON(proxyurl+urls, function(data){
+                        if(data.chart.result !== null) {
+                            if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500) {
+                                dat.push(data.chart.result[0].indicators.adjclose[0].adjclose);
+                                stock_data.push({exchange: exchange, ticker: ticker, description: description, data: dat});
+                                console.log(stock_data[stock_data.length-1].exchange);
+                                console.log(stock_data[stock_data.length-1].ticker);
+                                console.log(stock_data[stock_data.length-1].description);
+                                console.log(stock_data[stock_data.length-1].data[0].length);
+                            }                        
+                        } 
+                    });
+                arr.push(r);  
+                i++; 
+                }                
+            }                           
                             
                 
                  
@@ -72,6 +65,6 @@ function autorun() {
                     // // }
                 
             // }
-        // }
-    
+        }
+    });
 }
