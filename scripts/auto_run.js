f590d9a5110935c67e53ebf5af338771fd3c62f7
heri <40530246+heri@users.noreply.github.com>
2021-08-15 11:41:03 +0700

    update

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -23,16 +23,16 @@ function autorun() {
                     const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
                     const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+result.data[r].Symbol+"?symbol="+result.data[r].Symbol+"&period1=0&period2=9999999999&interval=1d";
                     $.getJSON(proxyurl+urls, function(data){
-                        if(data.chart.result !== null) {
-                            if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500) {
+                        // if(data.chart.result !== null) {
+                            // if(data.chart.result[0].indicators.adjclose[0].adjclose.length>2500) {
                                 dat.push(data.chart.result[0].indicators.adjclose[0].adjclose);
                                 stock_data.push({exchange: exchange, ticker: ticker, description: description, data: dat});
                                 console.log(stock_data[stock_data.length-1].exchange);
                                 console.log(stock_data[stock_data.length-1].ticker);
                                 console.log(stock_data[stock_data.length-1].description);
                                 console.log(stock_data[stock_data.length-1].data[0].length);
-                            }                        
-                        } 
+                        //     }                        
+                        // } 
                     });
                 arr.push(r);  
                 i++; 
