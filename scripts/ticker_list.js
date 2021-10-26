31e3fba8a7ee67f39d1f1b660f4e04627340dcfb
heri <40530246+heri@users.noreply.github.com>
2021-10-26 21:30:35 +0700

    update

---
+++ scripts/ticker_list.js
@@ -0,0 +1,186 @@
+var Euronext_Amsterdam = 
+    ["AALB.AS","ABN.AS","ACCEL.AS","ACOMO.AS","AF.AS","AGN.AS","AJAX.AS","AKZA.AS","AMG.AS","APAM.AS","ARCAD.AS","ASM.AS","ASML.AS","ATRS.AS","AXS.AS","BAMNB.AS","BBED.AS","BESI.AS","BEVER.AS","BOKA.AS","BOLS.AS","BRILL.AS","CLB.AS","CRBN.AS","CTAC.AS","DPA.AS","DSM.AS","ECMPA.AS","ESP.AS","FLOW.AS","FUR.AS","GLPG.AS","GROHA.AS","GVNV.AS","HAL.AS","HDG.AS","HEIA.AS","HEIO.AS","HOLCO.AS","HUNDP.AS","HYDRA.AS","ICT.AS","IEX.AS","IMCD.AS","INGA.AS","INTER.AS","KENDR.AS","KPN.AS","LVIDE.AS","MT.AS","NEDAP.AS","NEDSE.AS","NEWAY.AS","NN.AS","NOVI.AS","NSE.AS","NSI.AS","OCI.AS","ORANW.AS","ORDI.AS","PHARM.AS","PHIA.AS","PNL.AS","PORF.AS","PREVA.AS","RAND.AS","RDSA.AS","RDSB.AS","REN.AS","ROOD.AS","SBMO.AS","SLIGR.AS","SNOW.AS","STRN.AS","TBIRD.AS","TFG.AS","TIE.AS","TOM2.AS","TWEKA.AS","VALUE.AS","VPK.AS","VTA.AS","WKL.AS","YATRA.AS"];
+
+var American_Stock_Exchange = 
+    ["AAMC","AAU","ACU","ACY","AE","AINC","AIRI","AMPE","AMS","APT","ASM","ATNM","AUMN","AWX","AXU","BCV","BDL","BDR","BGI","BGSF","BHB","BHV","BLE","BLJ","BRG","BRN","BTG","BTN","BTSTA-TEST","BTX","BWL-A","CANF","CCF","CDOR","CEF","CET","CEV","CIK","CIX","CKX","CLM","CMT","CNR","CPHI","CQH","CQP","CRF","CRMD","CRVP","CSCR","CTO","CTP","CVM","CVR","CVU","DHY","DIT","DLA","DMF","DNN","DPW","DSS","DXR","EAD","ECF","EIA","EIM","EIO","EIP","ELLO","ELMD","EMAN","EMI","EMJ","ENSV","ENX","EPM","ERC","ERH","ESNC","ESP","EVBN","EVI","EVM","EVO","EVP","EVV","EVY","FAX","FCO","FEN","FRD","FSI","FSP","FTF","GBR","GGN","GGN-PB","GLO","GLQ","GLU","GLV","GORO","GPL","GRF","GSAT","GSS","GSV","GTE","GVP","HLM-P","HMG","HNW","HTM","HUSA","IAF","IBIO","IDI","IDN","IEC","IF","IGC","IHT","IMH","IMO","INFU","INS","INTT","INUV","IPB","ISDR","ISL","ISR","JOB","KIQ","KLDX","LAQ","LEU","LGL","LIQT","LNG","LODE","LOV","MAB","MAG","MCF","MDW","MHH","MIW","MLSS","MMV","MSN","MTNB","MVF","MXC","NAK","NAVB","NBH","NBO","NBW","NBY","NEN","NG","NGD","NHC","NHS","NML","NNVC","NOG","NRO","NSPR","NTIP","NYH","OCX","OGEN","PARR","PCG-PA","PCG-PB","PCG-PC","PCG-PD","PCG-PE","PCG-PG","PCG-PH","PCG-PI","PED","PLG","PLM","PLX","PRK","PTN","PW","PW-PA","PZG","RCG","REED","REI","RLGT","RVP","SAND","SBI","SDPI","SEB","SENS","SGA","SIF","SIM","SKY","SSY","STRP","STS","SVT","SYN","TGB","THM","TKAT","TMP","TPHS","TRT","TRX","TXMD","UAMY","UEC","UFAB","URG","UTG","UUU","UUUU","UWN","VCF","VFL","VGZ","VHC","VKI","VMM","VNRX","WRN","WTT","WYY","XPL","XTNT","XXII","YGRO"];
+
+var Australian_Stock_Exchange = 
+    ["1ST.AX","3PL.AX","88E.AX","8CO.AX","8EC.AX","8IH.AX","A2M.AX","AAA.AX","AAC.AX","AAJ.AX","AAP.AX","AAR.AX","AAU.AX","ABA.AX","ABC.AX","ABL.AX","ABP.AX","ABV.AX","ABX.AX","ACB.AX","ACP.AX","ACQ.AX","ACR.AX","ACS.AX","ACU.AX","ACW.AX","ADA.AX","ADD.AX","ADH.AX","ADN.AX","ADO.AX","ADR.AX","ADV.AX","ADX.AX","ADY.AX","AEE.AX","AEF.AX","AEG.AX","AEI.AX","AER.AX","AEV.AX","AFA.AX","AFG.AX","AFI.AX","AFP.AX","AFR.AX","AGD.AX","AGE.AX","AGG.AX","AGI.AX","AGJ.AX","AGL.AX","AGR.AX","AGS.AX","AGY.AX","AHF.AX","AHQ.AX","AHX.AX","AIA.AX","AIB.AX","AIQ.AX","AIR.AX","AIS.AX","AIV.AX","AIY.AX","AIZ.AX","AJJ.AX","AJL.AX","AJM.AX","AJQ.AX","AJX.AX","AJY.AX","AKG.AX","AKM.AX","AKP.AX","ALC.AX","ALI.AX","ALK.AX","ALL.AX","ALQ.AX","ALT.AX","ALU.AX","ALY.AX","AMA.AX","AMC.AX","AMH.AX","AMI.AX","AML.AX","AMN.AX","AMO.AX","AMP.AX","AMT.AX","ANG.AX","ANN.AX","ANO.AX","ANP.AX","ANR.AX","ANW.AX","ANZ.AX","AOA.AX","AON.AX","AOP.AX","AOU.AX","APA.AX","APD.AX","APE.AX","APG.AX","API.AX","APX.AX","APZ.AX","AQC.AX","AQD.AX","AQF.AX","AQI.AX","AQS.AX","AQX.AX","AQZ.AX","ARA.AX","ARB.AX","ARC.AX","ARD.AX","ARE.AX","ARF.AX","ARG.AX","ARO.AX","ARU.AX","ARV.AX","ASB.AX","ASH.AX","ASN.AX","ASP.AX","AST.AX","ASW.AX","ASX.AX","ATA.AX","ATC.AX","ATM.AX","ATP.AX","ATR.AX","ATU.AX","AUB.AX","AUC.AX","AUF.AX","AUP.AX","AUQ.AX","AUZ.AX","AVG.AX","AVH.AX","AVJ.AX","AVL.AX","AVN.AX","AVZ.AX","AWC.AX","AWN.AX","AWV.AX","AXE.AX","AXI.AX","AXT.AX","AYZ.AX","AZJ.AX","AZS.AX","AZY.AX","BAP.AX","BAR.AX","BBL.AX","BBN.AX","BBX.AX","BCC.AX","BCI.AX","BCK.AX","BCT.AX","BEL.AX","BEN.AX","BFC.AX","BFG.AX","BGA.AX","BGL.AX","BHP.AX","BIQ.AX","BIS.AX","BIT.AX","BKI.AX","BKL.AX","BKT.AX","BKW.AX","BKY.AX","BLD.AX","BLG.AX","BLX.AX","BLY.AX","BLZ.AX","BMG.AX","BMH.AX","BML.AX","BMN.AX","BNO.AX","BNR.AX","BOA.AX","BOC.AX","BOE.AX","BOL.AX","BOND.AX","BOQ.AX","BPH.AX","BPT.AX","BRB.AX","BRG.AX","BRK.AX","BRL.AX","BRN.AX","BRU.AX","BSA.AX","BSE.AX","BSL.AX","BSM.AX","BSR.AX","BTC.AX","BTI.AX","BUD.AX","BUX.AX","BUY.AX","BWF.AX","BWP.AX","BWX.AX","BXB.AX","BXN.AX","BYE.AX","BYI.AX","CAA.AX","CAF.AX","CAG.AX","CAJ.AX","CAM.AX","CAP.AX","CAQ.AX","CAR.AX","CAY.AX","CAZ.AX","CBA.AX","CBC.AX","CCJ.AX","CCP.AX","CCV.AX","CCZ.AX","CDA.AX","CDD.AX","CDG.AX","CDH.AX","CDM.AX","CDP.AX","CDT.AX","CDU.AX","CDY.AX","CEL.AX","CEN.AX","CGC.AX","CGF.AX","CGN.AX","CGO.AX","CGS.AX","CHC.AX","CHN.AX","CIA.AX","CIE.AX","CII.AX","CIM.AX","CIN.AX","CINPA.AX","CIW.AX","CIZ.AX","CKA.AX","CKF.AX","CL1.AX","CL8.AX","CLA.AX","CLH.AX","CLT.AX","CLV.AX","CLX.AX","CLZ.AX","CM8.AX","CMM.AX","CMP.AX","CMW.AX","CMY.AX","CNI.AX","CNL.AX","CNU.AX","CNW.AX","COE.AX","COH.AX","COI.AX","COO.AX","COY.AX","CPU.AX","CQR.AX","CRB.AX","CRL.AX","CRM.AX","CSE.AX","CSL.AX","CSR.AX","CSS.AX","CTD.AX","CTE.AX","CTM.AX","CTO.AX","CTP.AX","CUE.AX","CUP.AX","CUV.AX","CV1.AX","CVC.AX","CVN.AX","CVS.AX","CVV.AX","CVW.AX","CWN.AX","CWP.AX","CWY.AX","CXM.AX","CXO.AX","CXU.AX","CXX.AX","CXZ.AX","CYC.AX","CYG.AX","CYL.AX","CYP.AX","CZL.AX","CZN.AX","CZR.AX","CZZ.AX","D13.AX","DAF.AX","DCC.AX","DCG.AX","DCN.AX","DDR.AX","DDT.AX","DGH.AX","DGO.AX","DGR.AX","DGX.AX","DIV.AX","DJW.AX","DKM.AX","DME.AX","DMG.AX","DMP.AX","DNA.AX","DNK.AX","DOW.AX","DSX.AX","DTI.AX","DTL.AX","DTM.AX","DTR.AX","DUB.AX","DVL.AX","DVN.AX","DXB.AX","DXS.AX","DYL.AX","EAI.AX","EAS.AX","EAX.AX","EBG.AX","EBO.AX","ECG.AX","ECX.AX","EDE.AX","EEG.AX","EER.AX","EFE.AX","EGG.AX","EGH.AX","EGL.AX","EGN.AX","EGY.AX","EHE.AX","EHL.AX","ELD.AX","ELT.AX","EMB.AX","EME.AX","EMH.AX","EML.AX","EMR.AX","EMU.AX","ENN.AX","ENR.AX","ENT.AX","ENX.AX","EOL.AX","EOR.AX","EOS.AX","EPA.AX","EPD.AX","EPM.AX","EQE.AX","EQT.AX","EQX.AX","ERA.AX","ERL.AX","ERM.AX","ESK.AX","ESR.AX","EVE.AX","EVN.AX","EVO.AX","EVT.AX","EVZ.AX","EWC.AX","EZL.AX","FAR.AX","FBR.AX","FBU.AX","FCG.AX","FEL.AX","FFC.AX","FFG.AX","FFI.AX","FFT.AX","FGG.AX","FGR.AX","FGX.AX","FID.AX","FLN.AX","FLT.AX","FMG.AX","FML.AX","FMS.AX","FNP.AX","FNT.AX","FOD.AX","FPH.AX","FRI.AX","FRM.AX","FRX.AX","FSA.AX","FSF.AX","FSI.AX","FTZ.AX","FUN.AX","FWD.AX","FXJ.AX","FYI.AX","FZR.AX","GAP.AX","GBA.AX","GBE.AX","GBZ.AX","GC1.AX","GCR.AX","GCY.AX","GDF.AX","GDI.AX","GED.AX","GEM.AX","GES.AX","GFL.AX","GGE.AX","GGG.AX","GLA.AX","GLB.AX","GLE.AX","GLH.AX","GLL.AX","GLN.AX","GMA.AX","GMC.AX","GMD.AX","GME.AX","GMG.AX","GML.AX","GMN.AX","GMR.AX","GNC.AX","GNE.AX","GNG.AX","GNX.AX","GOLD.AX","GOR.AX","GOW.AX","GOZ.AX","GPR.AX","GPS.AX","GPT.AX","GRV.AX","GSS.AX","GTE.AX","GTG.AX","GTK.AX","GTR.AX","GUD.AX","GUL.AX","GVF.AX","GWA.AX","GWR.AX","GXY.AX","HAS.AX","HAV.AX","HAW.AX","HCH.AX","HCT.AX","HFR.AX","HGL.AX","HGO.AX","HHM.AX","HHY.AX","HIL.AX","HIT.AX","HLO.AX","HLS.AX","HLX.AX","HML.AX","HMX.AX","HNG.AX","HNR.AX","HPI.AX","HPR.AX","HRL.AX","HRR.AX","HSN.AX","HTA.AX","HUB.AX","HUO.AX","HVN.AX","HXG.AX","HZN.AX","HZR.AX","IAG.AX","IB8.AX","IBC.AX","IBG.AX","ICG.AX","ICI.AX","ICN.AX","ICQ.AX","ICS.AX","ICT.AX","IDT.AX","IDX.AX","IDZ.AX","IEC.AX","IEL.AX","IEM.AX","IFL.AX","IFM.AX","IFT.AX","IGL.AX","IGO.AX","ILU.AX","IMA.AX","IMC.AX","IMD.AX","IMU.AX","IPB.AX","IPC.AX","IPD.AX","IPE.AX","IPH.AX","IPL.AX","IPT.AX","IQ3.AX","IRD.AX","IRE.AX","IRI.AX","ISD.AX","ISU.AX","ITW.AX","IVC.AX","IVO.AX","IVR.AX","IVX.AX","IXR.AX","IZZ.AX","JAL.AX","JAT.AX","JBH.AX","JCS.AX","JHC.AX","JHX.AX","JIN.AX","JKA.AX","JPR.AX","JRL.AX","JRV.AX","JYC.AX","KAM.AX","KAR.AX","KAT.AX","KBC.AX","KCN.AX","KEY.AX","KFE.AX","KGD.AX","KGL.AX","KGM.AX","KIK.AX","KIN.AX","KIS.AX","KKL.AX","KKO.AX","KLO.AX","KLR.AX","KMD.AX","KME.AX","KNH.AX","KNM.AX","KNO.AX","KOR.AX","KOV.AX","KPO.AX","KPT.AX","KRL.AX","KRM.AX","KSC.AX","KSL.AX","KSN.AX","KTA.AX","KTE.AX","LAM.AX","LAU.AX","LBL.AX","LBT.AX","LCD.AX","LCE.AX","LCK.AX","LCT.AX","LCY.AX","LEG.AX","LEP.AX","LER.AX","LHM.AX","LIC.AX","LIO.AX","LIT.AX","LKE.AX","LLC.AX","LLO.AX","LMG.AX","LML.AX","LNK.AX","LOM.AX","LOV.AX","LPE.AX","LRS.AX","LSA.AX","LSR.AX","LSX.AX","LTR.AX","LVT.AX","LYC.AX","LYL.AX","MAH.AX","MAQ.AX","MAU.AX","MAX.AX","MBK.AX","MCE.AX","MCH.AX","MCP.AX","MCR.AX","MCT.AX","MCX.AX","MDC.AX","MDI.AX","MDR.AX","MEA.AX","MEB.AX","MEI.AX","MEP.AX","MEZ.AX","MFF.AX","MFG.AX","MGL.AX","MGR.AX","MGT.AX","MGU.AX","MGV.AX","MGX.AX","MHC.AX","MHI.AX","MIG.AX","MIH.AX","MIL.AX","MIN.AX","MIR.AX","MLD.AX","MLM.AX","MLT.AX","MLX.AX","MMI.AX","MMJ.AX","MML.AX","MMR.AX","MMS.AX","MND.AX","MNF.AX","MNS.AX","MNY.AX","MOC.AX","MOQ.AX","MOZ.AX","MP1.AX","MPL.AX","MPO.AX","MPP.AX","MPX.AX","MQG.AX","MRC.AX","MRD.AX","MRM.AX","MRQ.AX","MRR.AX","MSB.AX","MSG.AX","MSI.AX","MSM.AX","MSV.AX","MTB.AX","MTH.AX","MTN.AX","MTS.AX","MVE.AX","MVF.AX","MVP.AX","MVR.AX","MVW.AX","MX1.AX","MXC.AX","MXI.AX","MYE.AX","MYR.AX","MYS.AX","MYX.AX","NAB.AX","NAC.AX","NAG.AX","NAM.AX","NAN.AX","NCC.AX","NCK.AX","NCL.AX","NCM.AX","NDQ.AX","NEA.AX","NEC.AX","NEU.AX","NGE.AX","NGY.AX","NHC.AX","NHF.AX","NME.AX","NMI.AX","NML.AX","NMT.AX","NOR.AX","NOV.AX","NSE.AX","NSR.AX","NST.AX","NSX.AX","NTL.AX","NTU.AX","NUF.AX","NUH.AX","NWF.AX","NWH.AX","NWS.AX","NWSLV.AX","NXM.AX","NXT.AX","OAR.AX","OCC.AX","OCL.AX","ODN.AX","ODY.AX","OEC.AX","OEL.AX","OEQ.AX","OEX.AX","OFX.AX","OGC.AX","OIL.AX","OKJ.AX","OKU.AX","OLH.AX","OMH.AX","OML.AX","ONE.AX","ONT.AX","OOK.AX","OOO.AX","OPG.AX","OPT.AX","ORA.AX","ORE.AX","ORG.AX","ORI.AX","ORM.AX","ORN.AX","ORR.AX","OSH.AX","OSL.AX","OSP.AX","OTR.AX","OTW.AX","OXX.AX","OZG.AX","OZL.AX","PAA.AX","PAB.AX","PAC.AX","PAF.AX","PAI.AX","PAK.AX","PAN.AX","PAR.AX","PBP.AX","PCH.AX","PCL.AX","PDN.AX","PDZ.AX","PEC.AX","PEK.AX","PEL.AX","PEN.AX","PEX.AX","PFG.AX","PGC.AX","PGF.AX","PGH.AX","PGM.AX","PIC.AX","PIL.AX","PIQ.AX","PKA.AX","PLS.AX","PME.AX","PMV.AX","PNC.AX","PNL.AX","PNN.AX","PNR.AX","PNV.AX","PNX.AX","POS.AX","POW.AX","PPC.AX","PPG.AX","PPK.AX","PPL.AX","PPS.AX","PPT.AX","PPY.AX","PRT.AX","PRU.AX","PSA.AX","PSC.AX","PSI.AX","PSQ.AX","PTB.AX","PTL.AX","PTM.AX","PTR.AX","PTX.AX","PVE.AX","PWH.AX","PWN.AX","PXS.AX","PYC.AX","QAN.AX","QBE.AX","QHL.AX","QRE.AX","QST.AX","QTG.AX","QTM.AX","QUAL.AX","QUB.AX","QUE.AX","QVE.AX","R3D.AX","RAP.AX","RARI.AX","RBR.AX","RCE.AX","RCT.AX","RDG.AX","RDH.AX","RDM.AX","RDS.AX","REA.AX","RED.AX","REG.AX","REH.AX","RES.AX","REX.AX","REZ.AX","RFF.AX","RFG.AX","RFT.AX","RFX.AX","RGB.AX","RGS.AX","RHC.AX","RHI.AX","RHP.AX","RHT.AX","RIC.AX","RIE.AX","RIM.AX","RIO.AX","RKN.AX","RLC.AX","RMD.AX","RMG.AX","RMI.AX","RMS.AX","RMT.AX","RND.AX","RNL.AX","RNO.AX","RNT.AX","RNU.AX","RPG.AX","RRL.AX","RSG.AX","RSH.AX","RTG.AX","RTR.AX","RUL.AX","RVR.AX","RWD.AX","RXH.AX","RXL.AX","RXM.AX","RYD.AX","S2R.AX","S32.AX","SAN.AX","SAU.AX","SBM.AX","SBR.AX","SCG.AX","SCI.AX","SCP.AX","SCU.AX","SDF.AX","SDG.AX","SDL.AX","SEK.AX","SEN.AX","SER.AX","SES.AX","SFC.AX","SFG.AX","SFR.AX","SFX.AX","SGC.AX","SGF.AX","SGH.AX","SGM.AX","SGP.AX","SGQ.AX","SGR.AX","SGU.AX","SHE.AX","SHH.AX","SHJ.AX","SHL.AX","SHM.AX","SHU.AX","SHV.AX","SIH.AX","SIO.AX","SIQ.AX","SIT.AX","SIV.AX","SKC.AX","SKF.AX","SKI.AX","SKS.AX","SKT.AX","SLC.AX","SLK.AX","SLR.AX","SLX.AX","SMG.AX","SMI.AX","SMN.AX","SMP.AX","SMR.AX","SNC.AX","SND.AX","SNL.AX","SNZ.AX","SO4.AX","SOL.AX","SOM.AX","SOP.AX","SOR.AX","SPK.AX","SPL.AX","SPQ.AX","SPZ.AX","SRH.AX","SRI.AX","SRK.AX","SRV.AX","SRY.AX","SRZ.AX","SSL.AX","SSLPA.AX","SSM.AX","SSN.AX","SST.AX","STO.AX","STX.AX","SUD.AX","SUH.AX","SUL.AX","SUN.AX","SVM.AX","SVS.AX","SVW.AX","SVY.AX","SWK.AX","SWM.AX","SXE.AX","SXL.AX","SXY.AX","SYA.AX","SYD.AX","SYR.AX","TAH.AX","TAM.AX","TAR.AX","TAS.AX","TBR.AX","TCL.AX","TCO.AX","TDL.AX","TDO.AX","TFL.AX","TGA.AX","TGG.AX","TGN.AX","TGP.AX","TGR.AX","TGS.AX","TIG.AX","TLG.AX","TLM.AX","TLS.AX","TML.AX","TMX.AX","TNE.AX","TNK.AX","TNP.AX","TNR.AX","TOE.AX","TOM.AX","TON.AX","TOP.AX","TOU.AX","TPD.AX","TPO.AX","TPP.AX","TPS.AX","TPW.AX","TRL.AX","TRS.AX","TTA.AX","TTC.AX","TTI.AX","TWD.AX","TWE.AX","TWR.AX","TYX.AX","TZL.AX","TZN.AX","UBI.AX","UBN.AX","UCM.AX","UCW.AX","UOS.AX","URF.AX","UUL.AX","VAN.AX","VAR.AX","VBS.AX","VCX.AX","VELCP.AX","VGE.AX","VGL.AX","VIA.AX","VIG.AX","VIP.AX","VKA.AX","VMC.AX","VML.AX","VMS.AX","VMT.AX","VOC.AX","VRT.AX","VRX.AX","VTG.AX","VXR.AX","WAA.AX","WAF.AX","WAT.AX","WAX.AX","WBAPA.AX","WBC.AX","WCN.AX","WEB.AX","WEC.AX","WEL.AX","WES.AX","WGO.AX","WHF.AX","WKT.AX","WLD.AX","WNR.AX","WNS.AX","WOR.AX","WOW.AX","WPL.AX","WRM.AX","WSA.AX","WTP.AX","WWI.AX","XAM.AX","XF1.AX","XPD.AX","XRF.AX","XRO.AX","XST.AX","XTE.AX","XTV.AX","YAL.AX","YBR.AX","YOW.AX","YPB.AX","ZER.AX","ZEU.AX","ZGL.AX","ZIM.AX","ZNC.AX","ZTA.AX"];
+
+var Athens_Stock_Exchange = 
+    ["AEGN.AT","AKRIT.AT","ALMY.AT","ALPHA.AT","ANEK.AT","ANEPO.AT","ASCO.AT","ASTAK.AT","ATEK.AT","ATTICA.AT","AVAX.AT","AVE.AT","BELA.AT","BIOKA.AT","BIOSK.AT","BIOT.AT","BYTE.AT","CENTR.AT","CPI.AT","DAIOS.AT","DION.AT","DOMIK.AT","DROME.AT","EEE.AT","EKTER.AT","ELBE.AT","ELGEK.AT","ELIN.AT","ELPE.AT","ELSTR.AT","ELTON.AT","ENTER.AT","EPIL.AT","ETE.AT","EUPIC.AT","EUROB.AT","EVROF.AT","EXAE.AT","EYAPS.AT","EYDAP.AT","FIER.AT","FLEXO.AT","FOODL.AT","FOYRK.AT","FRIGO.AT","GEBKA.AT","GEKTERNA.AT","HAIDE.AT","HTO.AT","IASO.AT","IATR.AT","IKTIN.AT","ILYDA.AT","INKAT.AT","INLOT.AT","KAMP.AT","KARE.AT","KEKR.AT","KEPEN.AT","KLM.AT","KORDE.AT","KRI.AT","KTILA.AT","KYLO.AT","KYRI.AT","KYSA.AT","LAMDA.AT","LAMPS.AT","LANAC.AT","LAVI.AT","LEBEK.AT","LIVAN.AT","LOGISMOS.AT","LYK.AT","MATHIO.AT","MEDIC.AT","MERKO.AT","MEVA.AT","MOH.AT","MOTO.AT","MOYZK.AT","MPITR.AT","MYTIL.AT","NAKAS.AT","NAYP.AT","NEWS.AT","NIKAS.AT","OLTH.AT","OLYMP.AT","OPAP.AT","OTOEL.AT","PAP.AT","PETRO.AT","PLAIS.AT","PPA.AT","PPAK.AT","PPC.AT","PRESD.AT","PROF.AT","QUAL.AT","QUEST.AT","SAR.AT","SATOK.AT","SPACE.AT","TATT.AT","TELL.AT","TPEIR.AT","TRASTOR.AT","VIO.AT","VOSYS.AT","XYLEK.AT"];
+
+var Berlin_Stock_Exchange = 
+    [];
+
+var Euronext_Brussels = 
+    [];
+
+var Bombay_Stock_Exchange = 
+    [];
+
+var Buenos_Aires_Stock_Exchange = 
+    [];
+
+var Caracas_Stock_Exchange  = 
+    [];
+
+var Canadian_Securities_Exchange = 
+    [];
+
+var Nasdaq_OMX_Copenhagen = 
+    [];
+
+var Qatar_Stock_Exchange = 
+    [];
+
+var Dusseldorf_Stock_Exchange = 
+    [];
+
+var Swiss_Exchange = 
+    [];
+
+var Frankfurt_Stock_Exchange = 
+    [];
+
+var Deutsche_Boerse_XETRA = 
+    [];
+
+var Hamburg_Stock_Exchange = 
+    [];
+
+var Hanover_Stock_Exchange = 
+    [];
+
+var Nasdaq_OMX_Helsinki = 
+    [];
+
+var Hong_Kong_Stock_Exchange = 
+    [];
+
+var Nasdaq_OMX_Iceland = 
+    [];
+
+var London_Stock_Exchange = 
+    [];
+
+var Euronext_Dublin = 
+    [];
+
+var Borsa_Istanbul = 
+    [];
+
+var Indonesia_Stock_Exchange = 
+    [];
+
+var Malaysian_Stock_Exchange = 
+    [];
+
+var KOSDAQ = 
+    [];
+
+var Korea_Stock_Exchange = 
+    [];
+
+var Euronext_Lisbon = 
+    [];
+
+var Nasdaq_OMX_Vilnius = 
+    [];
+
+var London_Stock_Exchange = 
+    [];
+
+var Madrid_SE_CATS = 
+    [];
+
+var Moscow_Exchange = 
+    [];
+
+var Mexico_Stock_Exchange = 
+    [];
+
+var Italian_Stock_Exchange = 
+    [];
+
+var Munich_Stock_Exchange = 
+    [];
+
+var Nasdaq_Capital_Market = 
+    [];
+
+var Nasdaq_Global_Market = 
+    [];
+
+var Nasdaq_National_Market = 
+    [];
+
+var National_Stock_Exchange_of_India = 
+    [];
+
+var New_York_Stock_Exchange = 
+    [];
+
+var New_Zealand_Stock_Exchange = 
+    [];
+
+var Nasdaq_OTC_Bulletin_Board = 
+    [];
+
+var Oslo_Stock_Exchange = 
+    [];
+
+var Euronext_Paris = 
+    [];
+
+var NYSE_Arca = 
+    [];
+
+var OTC_Markets = 
+    [];
+
+var Nasdaq_OMX_Riga = 
+    [];
+
+var Sao_Paolo_Stock_Exchange = 
+    [];
+
+var Singapore_Stock_Exchange = 
+    [];
+
+var Stock_Exchange_of_Thailand = 
+    [];
+
+var Shanghai_Stock_Exchange  = 
+    [];
+
+var Shenzhen_Stock_Exchange = 
+    [];
+
+var Nasdaq_OMX_Stockholm = 
+    [];
+
+var Stuttgart_Stock_Exchange = 
+    [];
+
+var Taiwan_Stock_Exchange = 
+    [];
+
+var Nasdaq_OMX_Tallinn = 
+    [];
+
+var Tel_Aviv_Stock_Exchange = 
+    [];
+
+var Toronto_Stock_Exchange = 
+    [];
+
+var Taiwan_OTC_Exchange = 
+    [];
+
+var TSX_Venture_Exchange = 
+    [];
+
+var Vienna_Stock_Exchange = 
+    [];
+    
