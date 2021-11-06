97ba36f70cc4ae63b3a04ef37cd6967887fdb4be
heri <40530246+heri@users.noreply.github.com>
2021-11-07 06:57:29 +0700

    Update test_data.js

--- scripts/test_data.js
+++ scripts/test_data.js
@@ -233,41 +233,39 @@
             let as_data_price = new Array();
             let ex_choo = exchange_choose_current.split('-')[0];
 
-            const proxyurl = "https://api.codetabs.com/v1/proxy?quest=";
-            const urls = "https://query1.finance.yahoo.com/v8/finance/chart/"+tickere+"?symbol="+tickere+"&period1=0&period2=9999999999&interval=1d";
-
-            await $.getJSON(proxyurl+urls, function(result){
-              console.log(result);
-              var market_data = result;
-              let length_tm = market_data.chart.result[0].timestamp.length;
-              for(i=0; i<length_tm; i++) {
-              var data_date = new Date(market_data.chart.result[0].timestamp[i] * 1000);
-              var data_price = market_data.chart.result[0].indicators.adjclose[0].adjclose[i];             
+            await Papa.parse("dataset/"+exchange_choose_current+"/"+tickere+".csv", {
+              download: true,
+              header: false,
+              complete: function(result) {
+                console.log(result.data);
+                for(i=1; i<result.data.length; i++) {
+                  var data_date = new Date(result.data[i][0]);
+                  var data_price = result.data[i][5];
                   as_data_date.push(
                     appendLeadingZeroes(data_date.getMonth()+1) + "/" + appendLeadingZeroes(data_date.getDate()).toString().slice(0, 10) + "/" + data_date.getFullYear()
                   );
-                if(data_price == null) {
-                  as_data_price.push(
-                    as_data_price[as_data_price.length-1]
-                  );
-                } else {
-                  as_data_price.push(
-                    data_price
-                  );
-                }           
-              }
-              portfolio_data.push({ticker: tickere, data: {date: as_data_date, price: as_data_price}});
-              let al = portfolio_data.length;
-              let portfolio =            
-              `<tr>
-                  <td class="text-center">`+al+`</td>
-                  <td class="text-center">`+tickere+`</td>
-                  <td class="text-center">`+ex_choo+`</td>
-                  <td class="text-center">`+as_data_date[0]+`</td>
-              </tr>`;
-              $("#table_assets > tbody").append(portfolio);
-              });
-              // t++;         
+                   if(data_price == null) {
+                      as_data_price.push(
+                        as_data_price[as_data_price.length-1]
+                      );
+                    } else {
+                      as_data_price.push(
+                        data_price
+                      );
+                    }   
+                  }
+                  portfolio_data.push({ticker: tickere, data: {date: as_data_date, price: as_data_price}});
+                  let al = portfolio_data.length;
+                  let portfolio =            
+                  `<tr>
+                      <td class="text-center">`+al+`</td>
+                      <td class="text-center">`+tickere+`</td>
+                      <td class="text-center">`+ex_choo+`</td>
+                      <td class="text-center">`+as_data_date[0]+`</td>
+                  </tr>`;
+                  $("#table_assets > tbody").append(portfolio);
+                }
+              });        
           } 
           $(':button').prop('disabled', false);    
           
