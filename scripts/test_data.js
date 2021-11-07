35289e72436bd8fe558dbfc3d8657a787bcc0d91
heri <40530246+heri@users.noreply.github.com>
2021-11-07 11:59:22 +0700

    Update test_data.js

--- scripts/test_data.js
+++ scripts/test_data.js
@@ -82,7 +82,7 @@
     //   });
 
 
-    function add_data() {         
+    async function add_data() {         
       if(portfolio_data.length==30) {
         Swal.fire(
           '30 Stocks has been selected !',
@@ -116,7 +116,8 @@
         }
         $(':button').prop('disabled', true);    
         // var t = 0;
-        for (t=0;t < ticker_list.length && t < (30 - portfolio_data.length);t++) {          
+        for (t=0;t < ticker_list.length && t < (30 - portfolio_data.length);t++) { 
+          await asynchronousProcess();         
           let tickere = ticker_list[t].split(', ')[0];
           let as_data_date = new Array();
           let as_data_price = new Array();
