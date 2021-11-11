4a47207d3e9c8ca621ee1544704646bd6781dbe8
heri <40530246+heri@users.noreply.github.com>
2021-11-11 20:49:46 +0700

    Update run_test.js

--- scripts/run_test.js
+++ scripts/run_test.js
@@ -13,16 +13,16 @@
             )
         return false;
         } else {
-            // Swal.fire({
-            //     title: 'Connect to Quantxi AI',
-            //     text: 'You will be connected to the Quantxi AI Engine Version 1.1.5, although this is a simulation, you are connected to the same Quantxi intelligence.',
-            //     imageUrl: 'https://www.fundcalibre.com/wp-content/uploads/2020/04/AdobeStock_323829966-956x377.jpeg',
-            //     imageWidth: 400,
-            //     imageHeight: 200,
-            //     imageAlt: 'Custom image',
-            // })
+            Swal.fire({
+                title: 'Connect to Quantxi AI',
+                text: 'You will be connected to the Quantxi AI Engine Version 1.1.5, although this is a simulation, you are connected to the same Quantxi intelligence.',
+                imageUrl: 'https://www.fundcalibre.com/wp-content/uploads/2020/04/AdobeStock_323829966-956x377.jpeg',
+                imageWidth: 400,
+                imageHeight: 200,
+                imageAlt: 'Custom image',
+            })
             
-            // $(':button').prop('disabled', true); //Disable All Button
+            $(':button').prop('disabled', true); //Disable All Button
         }
 
         
@@ -50,7 +50,7 @@
         var daily_trade_summary = new Array();
 
         let i = 0;
-        while(i<7000) {
+        // while(i<7000) {
             i++;
             $('#data_id_input').html(i);
             $('#stock1_price').html(Intl.NumberFormat().format(parseFloat(1000+i).toFixed(2)));
@@ -175,7 +175,7 @@
             $('#stock29_signal_size').html(Intl.NumberFormat().format(parseFloat(123+i).toFixed(0)));
             $('#stock30_signal__position').html("BUY");
             $('#stock30_signal_size').html(Intl.NumberFormat().format(parseFloat(123+i).toFixed(0)));
-        }
+        // }
 
         while (data_id < test_data.length) { 
 
