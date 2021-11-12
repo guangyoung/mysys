48d75075185887f83b15bdf51057b3c8464dc33c
heri <40530246+heri@users.noreply.github.com>
2021-11-12 09:36:07 +0700

    Update run_test.js

--- scripts/run_test.js
+++ scripts/run_test.js
@@ -22,12 +22,22 @@
             //     imageAlt: 'Custom image',
             // })
 
-            Swal.fire(
-                'No Test Data !',
-                'Please click Market Data and Create It',
-                'warning'
-            )
-            return proses();
+            Swal.fire({
+                title: 'Connect to Quantxi AI',
+                text: 'You will be connected to the Quantxi AI Engine Version 1.1.5, although this is a simulation, you are connected to the same Quantxi intelligence.',
+                imageUrl: 'https://www.fundcalibre.com/wp-content/uploads/2020/04/AdobeStock_323829966-956x377.jpeg',
+                imageWidth: 400,
+                imageHeight: 200,
+                imageAlt: 'Custom image',
+                // showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                // cancelButtonColor: '#d33',
+                confirmButtonText: 'Ok'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    proses()
+                }
+              })           
         }
     }
 
