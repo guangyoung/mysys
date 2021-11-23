8e8a2aea7488da951dd74b6fafb6d10a662ccdec
heri <40530246+heri@users.noreply.github.com>
2021-11-23 10:02:24 +0700

    Update run_test.js

--- scripts/run_test.js
+++ scripts/run_test.js
@@ -62,9 +62,9 @@
                 if(result.isConfirmed) {                    
                     $(':button').prop('disabled', true); //Disable All Button                    
                     
-                    proses();
+                    // proses();
                     
-                    async function proses() {
+                    setInterval(async function() {
                         
                         if (total_post < test_data.length) {
                 
@@ -779,7 +779,7 @@
                             $('#quantxi_sortino').html(parseFloat((quantxi_sortino_ratio)*100).toFixed(2)+"%"); 
                             $('#buyandhold_sortino').html(parseFloat((buyandhold_sortino_ratio)*100).toFixed(2)+"%");  
                             
-                            setTimeout(proses, 1/10000); 
+                            // setTimeout(proses, 1/10000); 
                 
                         } else { 
                 
@@ -1004,7 +1004,7 @@
                             var ctx = document.getElementById('performance_chart').getContext('2d');
                             performance_chart = new Chart(ctx, config);
                                 
-                            clearTimeout();
+                            clearInterval();
                 
                             // $(':button').prop('disabled', false); //Enable All Button
                             $( "reset_button" ).prop( "disabled", false ); //Enable Reset Button
@@ -1019,7 +1019,7 @@
                                 'success'
                             )           
                         }           
-                    }
+                    },1);
                 }
             })           
         }
