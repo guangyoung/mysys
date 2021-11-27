de7d539597ee9f7e94610ea8212298ebd088064f
heri <40530246+heri@users.noreply.github.com>
2021-11-27 13:41:18 +0700

    Update run_test.js

--- scripts/run_test.js
+++ scripts/run_test.js
@@ -424,17 +424,13 @@ async function proses() {
         $("#inputElement").html(input_element);
 
         // var post_process = "run";
-        let apiKey = localStorage.getItem("apiKey");
-        let ur = "https://api.quantxi.com/post";
+        let ur = "https://api.quantxi.com/post?api_key=" + localStorage.getItem("apiKey");
 
         while (response_id < request_id) {
             await $.ajax({
                 type: "POST",
                 url: ur,
-                headers: {
-                    "x-api-key": apiKey
-                },
-                data: dataInput,                
+                data: dataInput,
                 dataType: 'json',
                 success: function (result) {
                     // console.log(result);
