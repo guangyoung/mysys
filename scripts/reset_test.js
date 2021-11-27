89c9f5b6dc247ea02dec654f4047e3117751e76a
heri <40530246+heri@users.noreply.github.com>
2021-11-27 12:12:56 +0700

    Update reset_test.js

--- scripts/reset_test.js
+++ scripts/reset_test.js
@@ -98,7 +98,7 @@ async function reset_test() {
   let ur = "https://api.quantxi.com/reset";
 
   await $.ajax({
-    type: "DELETE",
+    type: "PUT",
     url: ur,
     headers: {
         "x-api-key": apiKey
