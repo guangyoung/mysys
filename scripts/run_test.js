8aedae012953cedf533713ea2db7bc7f64a12bfb
heri <40530246+heri@users.noreply.github.com>
2021-11-27 20:38:45 +0700

    Update run_test.js

--- scripts/run_test.js
+++ scripts/run_test.js
@@ -430,9 +430,6 @@ async function proses() {
             await $.ajax({
                 type: "POST",
                 url: ur,
-                headers: {
-                    "X-SECRET-KEY": "asksksk" 
-                },
                 data: dataInput,
                 dataType: 'json',
                 success: function (result) {
