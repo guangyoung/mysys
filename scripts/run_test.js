84f25aae64e9587939daa8b80e4a7ceb140975d2
heri <40530246+heri@users.noreply.github.com>
2021-12-02 21:16:09 +0700

    Update run_test.js

--- scripts/run_test.js
+++ scripts/run_test.js
@@ -297,10 +297,10 @@ async function proses() {
             $("#position_stock"+i).html(eval(`Intl.NumberFormat().format(parseFloat(dataInput.stock`+i+`.position_size).toFixed(0))`));
         }
 
-        // var post_process = "run";
+        var post_process = "run";
         let ur = "https://api.quantxi.com/post?api_key=" + localStorage.getItem("apiKey");
 
-        while (dataInput.data_id > request_id) {
+        while (post_process = "run") {
             await $.ajax({
                 type: "POST",
                 url: ur,
@@ -446,6 +446,8 @@ async function proses() {
                         }                        
 
                         request_id++;
+
+                        post_process = "stop";
 
                         // ----------------------------------------------------------------------------------  
                         // TRADE TRANSACTION 
