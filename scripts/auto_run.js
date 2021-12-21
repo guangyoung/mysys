6508dc08206fdaca4c3a81397239091078975888
heri <40530246+heri@users.noreply.github.com>
2021-12-21 22:07:13 +0700

    Update auto_run.js

--- scripts/auto_run.js
+++ scripts/auto_run.js
@@ -26,7 +26,7 @@ function autorun() {
                                         let datee = new Date(data.chart.result[0].timestamp[i]*1000);
                                        
                                         datt.push(
-                                            datee,
+                                            data.chart.result[0].timestamp[i],
                                             data.chart.result[0].indicators.quote[0].open[i],
                                             data.chart.result[0].indicators.quote[0].high[i],
                                             data.chart.result[0].indicators.quote[0].low[i],
