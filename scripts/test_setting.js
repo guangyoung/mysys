e11b5bca57241d5fb595ac7435a9e3add9830be5
heri <40530246+heri@users.noreply.github.com>
2022-01-23 20:16:58 +0700

    update

--- scripts/test_setting.js
+++ scripts/test_setting.js
@@ -106,7 +106,7 @@
             $('#equity_with_loan_value').html(Intl.NumberFormat().format(parseFloat(initial_equity).toFixed(0)));
             $('#excess_liquidity').html(Intl.NumberFormat().format(parseFloat(initial_equity).toFixed(0)));
             $('#excess_equity').html(Intl.NumberFormat().format(parseFloat(initial_equity).toFixed(0)));
-            $('#SMA').html(Intl.NumberFormat().format(parseFloat(initial_equity).toFixed(0)));
+            $('#sma').html(Intl.NumberFormat().format(parseFloat(initial_equity).toFixed(0)));
             $('#marginBuying_power').html(Intl.NumberFormat().format(parseFloat(initial_equity*2).toFixed(0)));
 
             $('#setting').modal('hide');
