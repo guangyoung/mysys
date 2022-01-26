287184aa964bc9fc3fa5fbb2d5821448fc54b382
heri <40530246+heri@users.noreply.github.com>
2022-01-26 20:31:06 +0700

    Update test_data.js

--- scripts/test_data.js
+++ scripts/test_data.js
@@ -385,6 +385,7 @@ function change_period_ofTest() {
       'Please ganti tgl lain',
       'warning'
     )
+    console.log(startDate_test);
     $("#start_date_test").val(startDate_test.getFullYear() + "-" + appendLeadingZeroes(startDate_test.getMonth() + 1) + "-" + appendLeadingZeroes(startDate_test.getDate()));
     $("#end_date_test").val(endDate_test.getFullYear() + "-" + appendLeadingZeroes(endDate_test.getMonth() + 1) + "-" + appendLeadingZeroes(endDate_test.getDate()));
   } else {
