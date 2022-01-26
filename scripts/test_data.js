b7316dfcb8775a1ea5013bc55827f08e324c9eb7
heri <40530246+heri@users.noreply.github.com>
2022-01-26 13:44:00 +0700

    Update test_data.js

--- scripts/test_data.js
+++ scripts/test_data.js
@@ -380,7 +380,7 @@ function change_period_ofTest() {
   }
   // let startDate_test_idx = test_data[0].date.indexOf(date[0]"12/11/1995", 0);
   // let endDate_test_idx = test_data.date.indexOf(new Date(appendLeadingZeroes(endDate.getMonth() + 1) + "/" + appendLeadingZeroes(endDate.getDate()) + "/" + endDate.getFullYear()), startDate_test_idx);
-  // console.log(startDate_test_idx);
+  console.log(startDate_test_idx);
   // console.log(endDate_test_idx);
 }
 
