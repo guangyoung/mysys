8636f99ace7de58a5bd0f238d8c09dd154cb4c67
heri <40530246+heri@users.noreply.github.com>
2022-01-27 15:23:40 +0700

    Update test_data.js

--- scripts/test_data.js
+++ scripts/test_data.js
@@ -289,7 +289,7 @@ function create_test_data() {
     $("#enddate_data").val(appendLeadingZeroes(endDate.getMonth() + 1) + "/" + appendLeadingZeroes(endDate.getDate()) + "/" + endDate.getFullYear());
     $("#start_date_test").val(startDate_test.getFullYear() + "-" + appendLeadingZeroes(startDate_test.getMonth() + 1) + "-" + appendLeadingZeroes(startDate_test.getDate()));
     $("#end_date_test").val(endDate_test.getFullYear() + "-" + appendLeadingZeroes(endDate_test.getMonth() + 1) + "-" + appendLeadingZeroes(endDate_test.getDate()));
-    $("#start_date_test").attr("min", appendLeadingZeroes(startDate.getMonth() + 1) + "/" + appendLeadingZeroes(startDate.getDate()) + "/" + startDate.getFullYear());
+    // $("#start_date_test").attr("min", appendLeadingZeroes(startDate.getMonth() + 1) + "/" + appendLeadingZeroes(startDate.getDate()) + "/" + startDate.getFullYear());
     document.getElementById("start_date_test").setAttribute("min", "1995-12-11");
     console.log(startDate_test);
 
