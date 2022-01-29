a351a61487a49520b40df3e19718dacab7707430
heri <40530246+heri@users.noreply.github.com>
2022-01-29 20:55:53 +0700

    Update run_test.js

--- scripts/run_test.js
+++ scripts/run_test.js
@@ -366,8 +366,8 @@ async function run_test() {
             console.log("std: "+math.std(quantxi_total_return_array));
             console.log("252: "+math.sqrt(252));
 
-            quantxi_sharpe = ((math.mean(quantxi_total_return_array) - (risk_freeRate/360)) / math.std(quantxi_total_return_array))*math.sqrt(252);
-            buyhold_sharpe = ((math.mean(buyhold_total_return_array) - (risk_freeRate/360)) / math.std(buyhold_total_return_array))*math.sqrt(252);
+            quantxi_sharpe = ((math.mean(quantxi_total_return_array) - (risk_freeRate/360)) / math.std(quantxi_total_return_array));
+            buyhold_sharpe = ((math.mean(buyhold_total_return_array) - (risk_freeRate/360)) / math.std(buyhold_total_return_array));
 
             console.log("quantxi_sharpe: "+quantxi_sharpe);
             quantxi_sortino = (1);
