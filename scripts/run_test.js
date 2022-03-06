3e65f1a33309194770761c37d4572a52c0a890bb
heri <40530246+heri@users.noreply.github.com>
2022-03-06 14:50:26 +0700

    Update run_test.js

--- scripts/run_test.js
+++ scripts/run_test.js
@@ -278,14 +278,16 @@ async function run_test() {
 
         //calculate filled percentarge   
         let filled_percentage;
-        if((estimate_imr+estimate_comm) < 0) {
+        if((estimate_imr+estimate_comm) == 0) {
+            filled_percentage = 0;
+        }else if((estimate_imr+estimate_comm) < 0) {
             filled_percentage = 1;
         } else if(preTrade_excess_equity < 0 && (estimate_imr+estimate_comm) >= 0) {
             filled_percentage = 0;
         } else if(preTrade_excess_equity > (estimate_imr+estimate_comm)) {
             filled_percentage = 1;
         } else {
-            filled_percentage = (preTrade_excess_equity/(estimate_imr+estimate_comm));
+            filled_percentage = preTrade_excess_equity/(estimate_imr+estimate_comm);
         }
 
         console.log(filled_percentage);
