59797f349ec41cf8560ebeac06062ed318ac157e
guangyoung <40530246+guangyoung@users.noreply.github.com>
2023-12-17 14:54:49 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+    // try {  
+
+    //     $connect = mysqli_connect("127.0.0.1","root","","rkxcpixce4ozkxfd7kgctrcrwt5eknc7");
+    // } catch (mysqli_sql_exception $e) {
+    //     http_response_code(500);
+    //     $return = [
+    //       'status' 	=> 'Internal Server Error',
+    //       'message'	=> 'Mohon coba beberapa saat lagi'
+    //     ];
+    //   }
+    $ipp = "127.0.0.1";
+    $rpp = "root";
+    $tpp = "rkxcpixce4ozkxfd7kgctrcrwt5eknc7";
+?>
